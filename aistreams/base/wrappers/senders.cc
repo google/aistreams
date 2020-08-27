41f19b48b2378774d12ffe397628da8d58cf8d9a
Daniel Chao <dschao@google.com>
2020-08-27 00:08:11 -0700

    Adding public senders.
    + MakePacketSender is a wrapper that creates a PacketSender configured
      to operate on behalf of the public user. It provides a more controlled
      user model.
    
    Change-Id: I243666ac5d443cbbf84947c30df74d6ac002c49b

---
+++ aistreams/base/wrappers/senders.cc
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "aistreams/base/wrappers/senders.h"
+
+#include <functional>
+#include <thread>
+
+#include "absl/strings/str_format.h"
+#include "absl/time/time.h"
+#include "aistreams/base/connection_options.h"
+#include "aistreams/base/packet_sender.h"
+#include "aistreams/port/canonical_errors.h"
+#include "aistreams/port/status.h"
+#include "aistreams/port/status_macros.h"
+#include "aistreams/port/statusor.h"
+
+namespace aistreams {
+
+Status MakePacketSender(const SenderOptions& options,
+                        std::unique_ptr<PacketSender>* sender) {
+  // Create a PacketSender that uses uni-directional streaming gRPC.
+  PacketSender::Options packet_sender_options;
+  packet_sender_options.connection_options = options.connection_options;
+  packet_sender_options.stream_name = options.stream_name;
+  auto packet_sender_statusor = PacketSender::Create(packet_sender_options);
+  if (!packet_sender_statusor.ok()) {
+    LOG(ERROR) << packet_sender_statusor.status();
+    return UnknownError("Failed to create a PacketSender");
+  }
+  *sender = std::move(packet_sender_statusor).ValueOrDie();
+  return OkStatus();
+}
+
+}  // namespace aistreams
---
+++ aistreams/base/wrappers/senders.h
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef AISTREAMS_BASE_WRAPPERS_SENDERS_H_
+#define AISTREAMS_BASE_WRAPPERS_SENDERS_H_
+
+#include <functional>
+
+#include "aistreams/base/connection_options.h"
+#include "aistreams/base/packet.h"
+#include "aistreams/base/packet_sender.h"
+#include "aistreams/port/status.h"
+#include "aistreams/port/statusor.h"
+
+namespace aistreams {
+
+// Options to configure a sender.
+struct SenderOptions {
+  // Options to connect to the the service.
+  ConnectionOptions connection_options;
+
+  // The name of the stream to connect to.
+  std::string stream_name;
+};
+
+// Create a packet sender.
+//
+// The PacketSender created with this function is configured such that
+// PacketSender::Send(const Packet&) fails only when the underlying connection
+// is broken. If you wish to continue sending, you should arrange for
+// re-connections and backoffs; e.g. running MakePacketSender again.
+//
+// Normally, you should send a sequence of data Packets of the same type. When
+// you are done, make sure to send an EOS packet before destroying the
+// SenderQueue. You can create an EOS packet, e.g. using MakeEosPacket.
+//
+// TODO: At the moment PacketSender::Send() will not guarantee that the server
+// has actually accepted the packet. There are several ways forward, but this
+// is planned to arrive in beta/GA.
+Status MakePacketSender(const SenderOptions& options,
+                        std::unique_ptr<PacketSender>* sender);
+
+}  // namespace aistreams
+
+#endif  // AISTREAMS_BASE_WRAPPERS_SENDERS_H_
