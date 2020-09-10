6ce8bbebec61fca00f43c9da975d89096a2ad0eb
Yan Xue <yxyan@google.com>
2020-09-10 18:35:33 +0000

    Implement OnPremStreamManager.
    Change-Id: Ibf587b72679fb5e27fed05b79e44ad5eee9a3236

--- aistreams/base/management_client.cc
+++ aistreams/base/management_client.cc
@@ -18,63 +18,152 @@
 #include "aistreams/port/canonical_errors.h"
 #include "aistreams/port/status.h"
 #include "aistreams/port/status_macros.h"
+#include "aistreams/proto/management.grpc.pb.h"
 #include "aistreams/proto/management.pb.h"
 
 namespace aistreams {
 
-StatusOr<std::unique_ptr<ManagementClient>> ManagementClient::Create(
-    const ConnectionOptions& options) {
-  std::unique_ptr<ManagementClient> client(new ManagementClient(options));
-  AIS_RETURN_IF_ERROR(client->Initialize());
-  return client;
-}
-
-ManagementClient::ManagementClient(const ConnectionOptions& options)
-    : options_(options) {}
-
-Status ManagementClient::Initialize() {
-  auto grpc_channel = CreateGrpcChannel(options_);
-  if (grpc_channel == nullptr) {
-    return UnknownError("Failed to create a gRPC channel");
+// Stream manager for managing streams in on-prem cluster.
+class OnPremStreamManagerImpl : public StreamManager {
+ public:
+  static StatusOr<std::unique_ptr<StreamManager>> CreateStreamManager(
+      const StreamManagerOnPremConfig& config) {
+    auto manager_ptr = new OnPremStreamManagerImpl(config);
+    AIS_RETURN_IF_ERROR(manager_ptr->Initialize());
+    return std::unique_ptr<StreamManager>(manager_ptr);
   }
 
-  stub_ = ManagementServer::NewStub(grpc_channel);
-  if (stub_ == nullptr) {
-    return UnknownError("Failed to create a gRPC stub");
+  // CreateStream creates the stream. Return status to indicate whether the
+  // creation succeeded or failed.
+  virtual StatusOr<Stream> CreateStream(const Stream& stream) override {
+    grpc::ClientContext context;
+    AIS_RETURN_IF_ERROR(FillGrpcClientContext(options_.rpc_options, &context));
+
+    CreateStreamRequest request;
+    CreateStreamResponse response;
+    request.set_stream_name(stream.name());
+
+    grpc::Status grpc_status =
+        stub_->CreateStream(&context, request, &response);
+    if (!grpc_status.ok()) {
+      LOG(ERROR) << grpc_status.error_message();
+      return UnknownError("Encountered error calling RPC CreateStream");
+    }
+    return OkStatus();
   }
-  return OkStatus();
-}
 
-Status ManagementClient::CreateStream(const std::string& stream_name) {
-  grpc::ClientContext context;
-  AIS_RETURN_IF_ERROR(FillGrpcClientContext(options_.rpc_options, &context));
+  // DeleteStream deletes the stream. Return status to indicate whether the
+  // deletion succeeded or failed.
+  virtual Status DeleteStream(const std::string& stream_name) override {
+    grpc::ClientContext context;
+    AIS_RETURN_IF_ERROR(FillGrpcClientContext(options_.rpc_options, &context));
 
-  CreateStreamRequest request;
-  CreateStreamResponse response;
-  request.set_stream_name(stream_name);
+    DeleteStreamRequest request;
+    google::protobuf::Empty response;
+    request.set_stream_name(stream_name);
 
-  grpc::Status grpc_status = stub_->CreateStream(&context, request, &response);
-  if (!grpc_status.ok()) {
-    LOG(ERROR) << grpc_status.error_message();
-    return UnknownError("Encountered error calling RPC CreateStream");
+    grpc::Status grpc_status =
+        stub_->DeleteStream(&context, request, &response);
+    if (!grpc_status.ok()) {
+      LOG(ERROR) << grpc_status.error_message();
+      return UnknownError("Encountered error calling RPC DeleteStream");
+    }
+    return OkStatus();
   }
-  return OkStatus();
-}
 
-Status ManagementClient::DeleteStream(const std::string& stream_name) {
-  grpc::ClientContext context;
-  AIS_RETURN_IF_ERROR(FillGrpcClientContext(options_.rpc_options, &context));
+  // ListStreams lists streams. Return the list of streams if the request
+  // succeeds.
+  virtual StatusOr<std::vector<Stream>> ListStreams() override {
+    grpc::ClientContext context;
+    AIS_RETURN_IF_ERROR(FillGrpcClientContext(options_.rpc_options, &context));
 
-  DeleteStreamRequest request;
-  google::protobuf::Empty response;
-  request.set_stream_name(stream_name);
+    ListStreamRequest request;
+    ListStreamResponse response;
 
-  grpc::Status grpc_status = stub_->DeleteStream(&context, request, &response);
-  if (!grpc_status.ok()) {
-    LOG(ERROR) << grpc_status.error_message();
-    return UnknownError("Encountered error calling RPC DeleteStream");
+    grpc::Status grpc_status = stub_->ListStream(&context, request, &response);
+    if (!grpc_status.ok()) {
+      LOG(ERROR) << grpc_status.error_message();
+      return UnknownError("Encountered error calling RPC ListStreams");
+    }
+    std::vector<Stream> streams;
+    streams.reserve(response.stream_names_size());
+    for (const auto& stream_name : response.stream_names()) {
+      Stream stream;
+      stream.set_name(stream_name);
+      streams.push_back(stream);
+    }
+    return streams;
   }
-  return OkStatus();
+
+ private:
+  Status Initialize() {
+    auto grpc_channel = CreateGrpcChannel(options_);
+    if (grpc_channel == nullptr) {
+      return UnknownError("Failed to create a gRPC channel");
+    }
+
+    stub_ = ManagementServer::NewStub(grpc_channel);
+    if (stub_ == nullptr) {
+      return UnknownError("Failed to create a gRPC stub");
+    }
+    return OkStatus();
+  }
+
+  OnPremStreamManagerImpl(const StreamManagerOnPremConfig& config) {
+    options_.target_address = config.target_address();
+
+    // Setup ssl options.
+    options_.ssl_options.ssl_domain_name = config.ssl_domain_name();
+    options_.ssl_options.use_insecure_channel = config.use_insecure_channel();
+    options_.ssl_options.ssl_root_cert_path = config.ssl_root_cert_path();
+
+    // Setup rpc options.
+    options_.rpc_options.wait_for_ready = config.wait_for_ready();
+    options_.rpc_options.timeout = absl::Seconds(config.timeout().seconds()) +
+                                   absl::Nanoseconds(config.timeout().nanos());
+  }
+
+  ConnectionOptions options_;
+  std::unique_ptr<ManagementServer::Stub> stub_ = nullptr;
+};
+
+// Stream manager for managing streams in Google managed cluster.
+class ManagedStreamManagerImpl : public StreamManager {
+ public:
+  static StatusOr<std::unique_ptr<StreamManager>> CreateStreamManager(
+      const StreamManagerManagedConfig& config);
+
+  // CreateStream creates the stream. Return status to indicate whether the
+  // creation succeeded or failed.
+  virtual StatusOr<Stream> CreateStream(const Stream& stream) override;
+
+  // DeleteStream deletes the stream. Return status to indicate whether the
+  // deletion succeeded or failed.
+  virtual Status DeleteStream(const std::string& stream_name) override;
+
+  // ListStreams lists streams. Return the list of streams if the request
+  // succeeds.
+  virtual StatusOr<std::vector<Stream>> ListStreams() override;
+
+ private:
+  ManagedStreamManagerImpl(const StreamManagerManagedConfig& config);
+};
+
+StatusOr<std::unique_ptr<StreamManager>>
+StreamManagerFactory::CreateStreamManager(const StreamManagerConfig& config) {
+  if (config.has_stream_manager_onprem_config()) {
+    return OnPremStreamManagerImpl::CreateStreamManager(
+        config.stream_manager_onprem_config());
+  }
+
+  if (config.has_stream_manager_managed_config()) {
+    return ManagedStreamManagerImpl::CreateStreamManager(
+        config.stream_manager_managed_config());
+  }
+
+  return InvalidArgumentError(
+      "Input config is invalid. Either stream_manager_onprem_config or "
+      "stream_manager_managed_config should be specified.");
 }
 
 }  // namespace aistreams
--- aistreams/base/management_client.h
+++ aistreams/base/management_client.h
@@ -17,11 +17,8 @@
 #ifndef AISTREAMS_BASE_MANAGEMENT_CLIENT_H_
 #define AISTREAMS_BASE_MANAGEMENT_CLIENT_H_
 
-#include "aistreams/base/connection_options.h"
-#include "aistreams/port/canonical_errors.h"
 #include "aistreams/port/status.h"
 #include "aistreams/port/statusor.h"
-#include "aistreams/proto/management.grpc.pb.h"
 #include "aistreams/proto/management.pb.h"
 
 namespace aistreams {
@@ -50,90 +47,12 @@ class StreamManager {
   virtual StatusOr<std::vector<Stream>> ListStreams() = 0;
 };
 
-// Stream manager for managing streams in on-prem cluster.
-class OnPremStreamManagerImpl : public StreamManager {
- public:
-  static StatusOr<std::unique_ptr<StreamManager>> CreateStreamManager(
-      const StreamManagerOnPremConfig& config);
-
-  // CreateStream creates the stream. Return status to indicate whether the
-  // creation succeeded or failed.
-  virtual StatusOr<Stream> CreateStream(const Stream& stream) override;
-
-  // DeleteStream deletes the stream. Return status to indicate whether the
-  // deletion succeeded or failed.
-  virtual Status DeleteStream(const std::string& stream_name) override;
-
-  // ListStreams lists streams. Return the list of streams if the request
-  // succeeds.
-  virtual StatusOr<std::vector<Stream>> ListStreams() override;
-
- private:
-  OnPremStreamManagerImpl(const StreamManagerOnPremConfig& config);
-};
-
-// Stream manager for managing streams in Google managed cluster.
-class ManagedStreamManagerImpl : public StreamManager {
- public:
-  static StatusOr<std::unique_ptr<StreamManager>> CreateStreamManager(
-      const StreamManagerManagedConfig& config);
-
-  // CreateStream creates the stream. Return status to indicate whether the
-  // creation succeeded or failed.
-  virtual StatusOr<Stream> CreateStream(const Stream& stream) override;
-
-  // DeleteStream deletes the stream. Return status to indicate whether the
-  // deletion succeeded or failed.
-  virtual Status DeleteStream(const std::string& stream_name) override;
-
-  // ListStreams lists streams. Return the list of streams if the request
-  // succeeds.
-  virtual StatusOr<std::vector<Stream>> ListStreams() override;
-
- private:
-  ManagedStreamManagerImpl(const StreamManagerManagedConfig& config);
-};
-
 // Factory for creating stream manager.
 class StreamManagerFactory {
  public:
   // CreateStreamManager creates the stream manager given the
   static StatusOr<std::unique_ptr<StreamManager>> CreateStreamManager(
-      const StreamManagerConfig& config) {
-    if (config.has_stream_manager_onprem_config()) {
-      return OnPremStreamManagerImpl::CreateStreamManager(
-          config.stream_manager_onprem_config());
-    }
-
-    if (config.has_stream_manager_managed_config()) {
-      return ManagedStreamManagerImpl::CreateStreamManager(
-          config.stream_manager_managed_config());
-    }
-
-    return InvalidArgumentError(
-        "Input config is invalid. Either stream_manager_onprem_config or "
-        "stream_manager_managed_config should be specified.");
-  }
-};
-// Management client for creating/deleting/listing streams.
-//
-// TODO(yxyan): add interface for ManagementClient and add mock for
-// the client.
-class ManagementClient {
- public:
-  static StatusOr<std::unique_ptr<ManagementClient>> Create(
-      const ConnectionOptions& options);
-
-  Status CreateStream(const std::string& stream_name);
-
-  Status DeleteStream(const std::string& stream_name);
-
- private:
-  ManagementClient(const ConnectionOptions& options);
-  Status Initialize();
-
-  const ConnectionOptions options_;
-  std::unique_ptr<ManagementServer::Stub> stub_ = nullptr;
+      const StreamManagerConfig& config);
 };
 
 }  // namespace aistreams
