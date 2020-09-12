f328f0cb73690c1a7b75f64e776646c78a03c384
Daniel Chao <dschao@google.com>
2020-09-11 21:36:27 -0700

    Setuptools code to build a pip wheel.
    + build_pip_package will build a pip wheel when run from the WORKSPACE
      directory.
    + The pip package currently just contains aisctl. It will have client
      libraries later on if necessary. Users should be able to use aisctl
      from anywhere, without having to configure anything else like
      GST_PLUGIN_PATH etc. (this is handled in the package).
    
    Change-Id: Idc6a3dd57c0bf043d2071eedf80771e6773b48ce

---
+++ aistreams/python/pip_package/setup.py
@@ -0,0 +1,46 @@
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""AI Streams Python SDK."""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import setuptools
+
+VERSION = "0.0.1"
+REQUIRED_PACKAGES = []
+with open("requirements.txt", "r") as f:
+  REQUIRED_PACKAGES = f.read().splitlines()
+
+CONSOLE_SCRIPTS = ["aisctl = aistreams.python.apps.aisctl:main"]
+
+setuptools.setup(
+    name="aistreams",
+    version=VERSION,
+    author="Google Inc.",
+    author_email="",
+    description="AI Streams Python SDK",
+    long_description="AI Streams Python SDK",
+    long_description_content_type="text/markdown",
+    url="",
+    packages=setuptools.find_packages(),
+    install_requires=REQUIRED_PACKAGES,
+    entry_points={"console_scripts": CONSOLE_SCRIPTS},
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+    python_requires=">=3.6",
+)
