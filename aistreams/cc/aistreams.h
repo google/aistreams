6b70a346262dab6b46db0603ab591801a12daffb
Daniel Chao <dschao@google.com>
2020-09-11 21:07:54 -0700

    Added aistreams.h.
    + This header includes the full C++ API.
    + Users can include this single header.
    
    Change-Id: Ib7c6c49e5f186e90b45fdf775459bc21974c959b

---
+++ aistreams/cc/aistreams.h
@@ -0,0 +1,24 @@
+/*
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef AISTREAMS_CC_AISTREAMS_H_
+#define AISTREAMS_CC_AISTREAMS_H_
+
+#include "aistreams/cc/aistreams_lite.h"
+#include "aistreams/cc/decoded_receivers.h"
+#include "aistreams/cc/ingesters.h"
+
+#endif  // AISTREAMS_CC_AISTREAMS_H_
