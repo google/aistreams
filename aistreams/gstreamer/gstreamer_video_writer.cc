29c32b66c1f457f75d40725a98ca58d82c66b5c8
Daniel Chao <dschao@google.com>
2020-12-07 22:32:26 -0800

    A class whose instances write video files.
    GstreamerVideoWriter writes a video file from the sequence of
    GstreamerBuffers that it is given. We may write video files from
    Packets directly in the future (which will include all the packet
    type validations), but we can start with this interface first.
    
    Each instances writes a single file; chunking of video boundaries from
    the stream is up to the client.
    
    Change-Id: Ib5ca8891681259a3cb9cdbf2620559730f7a1e79

---
+++ aistreams/gstreamer/gstreamer_video_writer.cc
@@ -0,0 +1,101 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "aistreams/gstreamer/gstreamer_video_writer.h"
+
+#include "absl/strings/str_format.h"
+#include "absl/strings/str_join.h"
+#include "aistreams/gstreamer/type_utils.h"
+#include "aistreams/port/canonical_errors.h"
+#include "aistreams/port/logging.h"
+#include "aistreams/port/status.h"
+#include "aistreams/port/status_macros.h"
+
+namespace aistreams {
+
+namespace {
+
+Status ValidateOptions(const GstreamerVideoWriter::Options& options) {
+  if (options.filename.empty()) {
+    return InvalidArgumentError(
+        "You must supply the name of the output video file");
+  }
+
+  if (options.caps_string.empty()) {
+    return InvalidArgumentError(
+        "You must supply the expected caps string of the incoming gstreamer "
+        "buffers");
+  }
+  return OkStatus();
+}
+
+StatusOr<std::string> AssembleGstreamerPipeline(
+    const GstreamerVideoWriter::Options& options) {
+  std::vector<std::string> pipeline_elements;
+  pipeline_elements.push_back("decodebin");
+  pipeline_elements.push_back("videoconvert");
+  pipeline_elements.push_back("x264enc");
+  pipeline_elements.push_back("mp4mux");
+  pipeline_elements.push_back(
+      absl::StrFormat("filesink location=%s", options.filename));
+  return absl::StrJoin(pipeline_elements, " ! ");
+}
+
+}  // namespace
+
+GstreamerVideoWriter::GstreamerVideoWriter(const Options& options)
+    : options_(options) {}
+
+StatusOr<std::unique_ptr<GstreamerVideoWriter>> GstreamerVideoWriter::Create(
+    const Options& options) {
+  AIS_RETURN_IF_ERROR(ValidateOptions(options));
+
+  auto video_writer = std::make_unique<GstreamerVideoWriter>(options);
+  auto status = video_writer->Initialize();
+  if (!status.ok()) {
+    LOG(ERROR) << status;
+    return InternalError("Failed to Initialize the GstreamerVideoWriter");
+  }
+  return video_writer;
+}
+
+Status GstreamerVideoWriter::Initialize() {
+  // Assemble the main gstreamer processing pipeline string.
+  auto pipeline_string_statusor = AssembleGstreamerPipeline(options_);
+  if (!pipeline_string_statusor.ok()) {
+    return pipeline_string_statusor.status();
+  }
+  auto pipeline_string = std::move(pipeline_string_statusor).ValueOrDie();
+
+  // Create a GstreamerRunner that can be fed.
+  GstreamerRunner::Options gstreamer_runner_options;
+  gstreamer_runner_options.appsrc_caps_string = options_.caps_string;
+  gstreamer_runner_options.processing_pipeline_string = pipeline_string;
+
+  auto gstreamer_runner_statusor =
+      GstreamerRunner::Create(gstreamer_runner_options);
+  if (!gstreamer_runner_statusor.ok()) {
+    LOG(ERROR) << gstreamer_runner_statusor.status();
+    return UnknownError("Failed to create the GstreamerRunner");
+  }
+  gstreamer_runner_ = std::move(gstreamer_runner_statusor).ValueOrDie();
+
+  return OkStatus();
+}
+
+Status GstreamerVideoWriter::Put(const GstreamerBuffer& gstreamer_buffer) {
+  return gstreamer_runner_->Feed(gstreamer_buffer);
+}
+
+}  // namespace aistreams
---
+++ aistreams/gstreamer/gstreamer_video_writer.h
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef AISTREAMS_GSTREAMER_GSTREAMER_VIDEO_WRITER_H_
+#define AISTREAMS_GSTREAMER_GSTREAMER_VIDEO_WRITER_H_
+
+#include <functional>
+#include <memory>
+
+#include "aistreams/base/types/gstreamer_buffer.h"
+#include "aistreams/base/types/raw_image.h"
+#include "aistreams/gstreamer/gstreamer_runner.h"
+#include "aistreams/port/status.h"
+#include "aistreams/port/statusor.h"
+
+namespace aistreams {
+
+// This class writes a single video file from frames it receives.
+class GstreamerVideoWriter {
+ public:
+  // Options to configure the GstreamerVideoWriter.
+  struct Options {
+    // The name of the output video file.
+    std::string filename;
+
+    // The caps string of all gstreamer buffers that would be fed.
+    std::string caps_string;
+  };
+
+  // Create an instance in a fully initialized state.
+  static StatusOr<std::unique_ptr<GstreamerVideoWriter>> Create(const Options&);
+
+  // Add a gstreamer buffer into the output video.
+  //
+  // It must have the same caps as that specified in Options.
+  Status Put(const GstreamerBuffer&);
+
+  // Copy-control members. Use Create() rather than the constructors.
+  ~GstreamerVideoWriter();
+  explicit GstreamerVideoWriter(const Options&);
+  GstreamerVideoWriter() = delete;
+  GstreamerVideoWriter(const GstreamerVideoWriter&) = delete;
+  GstreamerVideoWriter& operator=(const GstreamerVideoWriter&) = delete;
+  GstreamerVideoWriter(GstreamerVideoWriter&&) = delete;
+  GstreamerVideoWriter& operator=(GstreamerVideoWriter&&) = delete;
+
+ private:
+  Status Initialize();
+
+  Options options_;
+  std::unique_ptr<GstreamerRunner> gstreamer_runner_ = nullptr;
+};
+
+}  // namespace aistreams
+
+#endif  // AISTREAMS_GSTREAMER_GSTREAMER_VIDEO_WRITER_H_
